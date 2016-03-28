2eac7c3e3b9d8cde3521675451e9a57a18a63fdf
dima.vorona <zen.dev@yandex.com>
2016-03-28 11:05:05 +0300

    Manual merge of https://github.com/romaonthego/RESideMenu/pull/244

--- RESideMenu/RESideMenu.m
+++ RESideMenu/RESideMenu.m
@@ -411,6 +411,8 @@
             return;
         }
         [visibleMenuViewController endAppearanceTransition];
+        [self statusBarNeedsAppearanceUpdate];
+
         if (!strongSelf.visible && [strongSelf.delegate conformsToProtocol:@protocol(RESideMenuDelegate)] && [strongSelf.delegate respondsToSelector:@selector(sideMenu:didHideMenuViewController:)]) {
             [strongSelf.delegate sideMenu:strongSelf didHideMenuViewController:rightMenuVisible ? strongSelf.rightMenuViewController : strongSelf.leftMenuViewController];
         }
@@ -428,7 +430,6 @@
         animationBlock();
         completionBlock();
     }
-    [self statusBarNeedsAppearanceUpdate];
 }
 
 - (void)addContentButton
